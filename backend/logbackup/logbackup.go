18bb830bae0422803502bd5f8c22de2f7a9b89c5
ryancartoon <ryancartoon@outlook.com>
2024-12-15 22:31:34 +0800

    update log backup

--- backend/logbackup/logbackup.go
+++ backend/logbackup/logbackup.go
@@ -1,6 +1,7 @@
 package logbackup
 
 import (
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"io"
@@ -22,6 +23,7 @@ func init() {
 type DB interface {
 	AddFileMeta(FileMeta FileMeta) error
 	UpdateLastUpdateTime(t time.Time) error
+	GetLastUpdateTime() (time.Time, error)
 }
 
 type FileMeta struct {
@@ -35,30 +37,46 @@ type FileMeta struct {
 	UpdateAt  time.Time
 }
 
-func NewWatcher() *LogWatcher {
+func calculateSHA256(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hasher.Sum(nil)), nil
+}
+
+func NewLogWatcher(db DB) *LogWatcher {
 	fsWatcher, err := fsnotify.NewWatcher()
 
 	if err != nil {
 		panic("Failed to create watcher")
 	}
 
-	filemetaCh := make(chan FileMeta)
+	newFileMetaCh := make(chan FileMeta)
 
 	return &LogWatcher{
-		watcher:    fsWatcher,
-		fileMetaCh: filemetaCh,
-		stoppingCh: make(chan struct{}),
-		addPathCh:  make(chan string),
+		db:            db,
+		watcher:       fsWatcher,
+		newFileMetaCh: newFileMetaCh,
+		stoppingCh:    make(chan struct{}),
+		addPathCh:     make(chan string),
 	}
 }
 
 type LogWatcher struct {
-	watcher      *fsnotify.Watcher
-	fileMetaCh   chan FileMeta
-	stoppingCh   chan struct{}
-	addPathCh    chan string
-	removePathCh chan string
-	db           DB
+	watcher       *fsnotify.Watcher
+	newFileMetaCh chan FileMeta
+	stoppingCh    chan struct{}
+	addPathCh     chan string
+	removePathCh  chan string
+	db            DB
 }
 
 func (w *LogWatcher) AddPath(path string) error {
@@ -118,8 +136,8 @@ func (w *LogWatcher) beforeWatch(paths []string) error {
 	return nil
 }
 
-func (w *LogWatcher) Watch() error {
-	err := w.beforeWatch()
+func (w *LogWatcher) Watch(ctx context.Context) error {
+	err := w.beforeWatch([]string{})
 
 	if err != nil {
 		return err
@@ -159,11 +177,11 @@ RunningLoop:
 						SHA256:  sha256Sum,
 					}
 
-					w.fileMetaCh <- fm
+					w.newFileMetaCh <- fm
 
 				}
 			}
-		case fm := <-w.fileMetaCh:
+		case fm := <-w.newFileMetaCh:
 			if err := w.AddNewFile(fm); err != nil {
 				logger.Errorf("%v", err)
 			}
@@ -174,15 +192,22 @@ RunningLoop:
 		case <-time.After(5 * time.Second):
 			logger.Info("heart beat")
 
-		case err, _ := <-w.watcher.Errors:
-			logger.Printf("Watcher error: %v", err)
+		case err, ok := <-w.watcher.Errors:
+			if !ok {
+				logger.Error("watcher closed")
+			}
+			logger.Errorf("error: %s", err)
 		case <-w.stoppingCh:
+			w.watcher.Close()
 			break RunningLoop
+		case <-ctx.Done():
+			w.watcher.Close()
+			break RunningLoop
+
 		}
 	}
 
 	return nil
-
 }
 
 func (w *LogWatcher) AddNewFile(file FileMeta) error {
--- backend/logbackup/logbackup_test.go
+++ backend/logbackup/logbackup_test.go
@@ -1,1 +1,71 @@
 package logbackup
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type dbMock struct {
+	lastUpdateTime time.Time
+	fileMetas      []FileMeta
+}
+
+func (m *dbMock) AddFileMeta(meta FileMeta) error {
+	m.fileMetas = append(m.fileMetas, meta)
+	return nil
+}
+
+func (m *dbMock) UpdateLastUpdateTime(lastUpdateTime time.Time) error {
+	return nil
+}
+
+func (m *dbMock) GetLastUpdateTime() (time.Time, error) {
+	return m.lastUpdateTime, nil
+}
+
+func TestWatcherWatch(t *testing.T) {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	// This should involve creating a mock database and watcher,
+	// setting up a test directory with files, starting the watcher, and then
+	// making sure it detects the file changes and updates the database accordingly.
+
+	// Set up a test directory with some files.
+	testDir1 := t.TempDir()
+	testDir2 := t.TempDir()
+	createTestFiles(testDir1, []string{"1"})
+	createTestFiles(testDir2, []string{"1"})
+
+	db := &dbMock{}
+	watcher := NewLogWatcher(db)
+	err := watcher.AddPath(testDir1)
+	assert.NoError(t, err)
+	err = watcher.AddPath(testDir2)
+	assert.NoError(t, err)
+
+	go watcher.Watch(ctx)
+
+	time.Sleep(time.Second * 1)
+
+	assert.Equal(t, len(db.fileMetas), 0) // Assuming two files were created initially.
+
+	createTestFiles(testDir1, []string{"2"})
+	time.Sleep(time.Second * 20)
+	assert.Equal(t, len(db.fileMetas), 1)
+
+	// createTestFiles(testDir2, []string{"2"})
+	// time.Sleep(time.Second * 1)
+	// assert.Equal(t, len(db.fileMetas), 2)
+}
+
+func createTestFiles(dir string, fileNames []string) {
+	for _, fileName := range fileNames {
+		os.WriteFile(filepath.Join(dir, fileName), []byte("test content"), 0644)
+	}
+}
